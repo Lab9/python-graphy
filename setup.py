7261f1af57cfdc7d61bd459b52f41e826205d373
taaseda8 <daniel.seifert@swisscom.com>
2020-07-20 10:30:32 +0200

    increase readability and add keywords

--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@ import pathlib
 from setuptools import setup
 
 # The directory containing this file
-HERE = pathlib.Path(__file__).parent
+root_directory = pathlib.Path(__file__).parent
 
 # The text of the README file
-README = (HERE / "README.md").read_text()
+readme = (root_directory / "README.md").read_text(encoding="UTF-8")
 
 # This call to setup() does all the work
 setup(
     name="python-graphy",
     version="0.0.2",
     description="A fast and modern graphql client library designed with simplicity in mind.",
-    long_description=README,
+    long_description=readme,
     long_description_content_type="text/markdown",
     url="https://github.com/Lab9/python-graphy",
     author="Daniel Seifert",
@@ -24,6 +24,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
     ],
+    keywords="python graphql client api",
     packages=["graphy"],
     include_package_data=True,
     install_requires=["requests"],
