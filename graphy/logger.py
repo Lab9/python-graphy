5af65c76df9f3da97a3ddc3546a15b484894aa17
Daniel Seifert <info@danielseifert.ch>
2020-08-08 14:25:26 +0200

    removed unnecessary create logger function

--- graphy/logger.py
+++ graphy/logger.py
@@ -1,14 +1,3 @@
-import sys
-from logging import Logger, getLogger, Formatter, StreamHandler
+from logging import getLogger
 
-
-def __create_package_logger() -> 'Logger':
-    __logger = getLogger("graphy")
-    formatter = Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    stdout_handler = StreamHandler(sys.stdout)
-    stdout_handler.setFormatter(formatter)
-    __logger.addHandler(stdout_handler)
-    return __logger
-
-
-logger = __create_package_logger()
+logger = getLogger("graphy")
